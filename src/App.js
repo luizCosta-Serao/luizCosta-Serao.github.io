2ea74e2ee3d95adcc3e96263de9e202af29d763f
Luiz Serão <luiz.asdc@hotmail.com>
2022-11-17 16:57:43 -0300

    Skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About/About';
 import Header from './components/Header';
 import Home from './components/Home/Home';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Skills />
     </>
   );
 }
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styles from "./Skills.module.css"
+
+const Skills = () => {
+  return (
+    <section className={`${styles.skills} container`}>
+      <h1>Skills</h1>
+      <p>Meu nível técnico</p>
+      <div className={styles.frontEnd}>
+        <h2>Front End</h2>
+        <ul className={styles.skillsList}>
+          <li>
+            <p>HTML</p>
+            <span>Avançado</span>
+          </li>
+          <li>
+            <p>CSS</p>
+            <span>Avançado</span>
+          </li>
+          <li>
+            <p>JavaScript</p>
+            <span>Intermediário</span>
+          </li>
+          <li>
+            <p>React</p>
+            <span>Intermediário</span>
+          </li>
+          <li>
+            <p>Wordpress</p>
+            <span>Intermediário</span>
+          </li>
+          <li>
+            <p>Figma</p>
+            <span>Intermediário</span>
+          </li>
+          <li>
+            <p>Bootstrap</p>
+            <span>Básico</span>
+          </li>
+          <li>
+            <p>sass</p>
+            <span>Básico</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
