215c51a21b5fc0c7ef5ef0029e7b274d00f5d81c
Luiz Serão <luiz.serao@hotmail.com>
2022-12-19 08:23:44 -0300

    Substituição das imagens dos projetos da section portólio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import Slide from './Slide'
 import styles from "./Portfolio.module.css"
-import dogs from "../../imgs/Portfolio/portfolio_dog.png"
+import dogs from "../../imgs/Portfolio/portfolio-dog.png"
 import desafioRouter from "../../imgs/Portfolio/portfolio_products.png"
 import wildBeast from "../../imgs/Portfolio/projeto-wildbeast.png"
-import animaisFantasticos from "../../imgs/Portfolio/project-animais-fantasticos.png"
 import sunnyside from "../../imgs/Portfolio/sunnyside.png"
+import formIncricao from "../../imgs/Portfolio/form-portfolio.png"
 
 const Portfolio = () => {
   const slides =  [
     {
-      text:"Projeto realizado com API REST E ReactJS",
+      text:"Projeto de Rede Social para cachorros realizado com API REST, ReactJS, HTML, CSS e JavaScript",
       img:<img src={dogs} alt="dogs"/>,
       href:"https://luizcosta-serao.github.io/dogs-react/"
     },
     {
-      text:"Projeto realizado com ReactJS e API",
+      text:"Projeto de Produtos Eletrônicos realizado com ReactJS, HTML, CSS, JavaScript e API",
       img:<img src={desafioRouter} alt="router"/> ,
       href:"https://luizcosta-serao.github.io/desafio-router-react/"
     },
     {
-      text:"Projeto realizado com HTML, CSS e JavaScript",
+      text:"Projeto Sunnyside Agency realizado com HTML, CSS e JavaScript",
       img:<img src={sunnyside} alt="agência sunnyside"/>,
       href:"https://luizcosta-serao.github.io/LP_Sunnyside_Agency/"
     },
     {
-      text:"Projeto realizado com HTML e CSS",
+      text:"Projeto Wildbeast realizado com HTML e CSS",
       img:<img src={wildBeast} alt="wildbeast"/>,
       href:"https://luizcosta-serao.github.io/Wildbeast-Grid/"
     },
     {
-      text:"Projeto realizado com HTML CSS e JavaScript",
-      img:<img src={animaisFantasticos} alt="animais fantásticos"/>,
-      href:"https://luizcosta-serao.github.io/animais-fantasticos/"
+      text:"Projeto de formulário com validação do lado do cliente com HTML, CSS e JavaScript utilizando regex",
+      img:<img src={formIncricao} alt="Formulário de inscrição"/>,
+      href:"https://luizcosta-serao.github.io/formulario-inscricao/"
     }
   ]
 
