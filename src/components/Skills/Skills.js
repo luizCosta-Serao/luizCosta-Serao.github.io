c3f6af49aea9371af3a42da919c8d65829da91ec
Luiz Serão <luiz.serao@hotmail.com>
2024-01-15 15:16:23 -0300

    Atualização da seção Skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,53 +1,52 @@
 import React from 'react'
 import styles from "./Skills.module.css"
+import html from '../../imgs/Skills/html.svg'
+import css from '../../imgs/Skills/css.svg'
+import js from '../../imgs/Skills/javascript.svg'
+import figma from '../../imgs/Skills/figma.svg'
+import react from '../../imgs/Skills/reactjs.svg'
+import sass from '../../imgs/Skills/sass.svg'
+import styledComponents from '../../imgs/Skills/styled-components.svg'
+import github from '../../imgs/Skills/github.svg'
 
 const Skills = () => {
   return (
     <section id='skills' className={`${styles.skills} container`}>
       <h1>Skills</h1>
-      <p>Meu nível técnico</p>
       <div className={styles.frontEnd}>
         <h2>Front End</h2>
         <ul className={styles.skillsList}>
           <li>
             <p>HTML</p>
-            <span>Avançado</span>
+            <img src={html} alt='HTML' />
           </li>
           <li>
             <p>CSS</p>
-            <span>Avançado</span>
+            <img src={css} alt='CSS' />
           </li>
           <li>
             <p>JavaScript</p>
-            <span>Intermediário</span>
+            <img src={js} alt='JavaScript'/>
           </li>
           <li>
             <p>React</p>
-            <span>Intermediário</span>
-          </li>
-          <li>
-            <p>Wordpress</p>
-            <span>Intermediário</span>
+            <img src={react} alt='ReactJS' />
           </li>
           <li>
             <p>Figma</p>
-            <span>Intermediário</span>
+            <img src={figma} alt='Figma' />
           </li>
           <li>
-            <p>Bootstrap</p>
-            <span>Básico</span>
+            <p>Sass</p>
+            <img src={sass} alt='SASS' />
           </li>
           <li>
-            <p>sass</p>
-            <span>Básico</span>
+            <p>Styled Components</p>
+            <img src={styledComponents} alt='Styled Components' />
           </li>
           <li>
-            <p>NodeJS</p>
-            <span>Iniciante</span>
-          </li>
-          <li>
-            <p>MongoDB</p>
-            <span>Intermediário</span>
+            <p>Github</p>
+            <img src={github} alt='Github' />
           </li>
         </ul>
       </div>
