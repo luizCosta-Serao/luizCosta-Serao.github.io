605a9014facd328d6fcacc289626e9665087c3c0
Luiz Serão <luiz.serao@hotmail.com>
2024-01-16 09:28:02 -0300

    destaque de uma parte do texto em Sobre

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
       <img className='animationImage' src={photoAbout} alt="Foto do Sobre"/>
       <div className={`${styles.content}`}>
         <p>Olá! Sou um desenvolvedor front-end apaixonado por criar experiências incríveis na web. Com foco em React, estou sempre em busca de novos desafios e oportunidades para aprimorar minhas habilidades e conhecimentos.</p>
-        <p>Se você está procurando um desenvolvedor front-end React apaixonado e comprometido, estou pronto para fazer parte do seu time!</p>
+        <p className={styles.job}>Se você está procurando um desenvolvedor front-end React apaixonado e comprometido, estou pronto para fazer parte do seu time!</p>
         <a className={styles.curriculo} download="curriculo" href={curriculo}>
           <Button>Download CV</Button>
         </a>
