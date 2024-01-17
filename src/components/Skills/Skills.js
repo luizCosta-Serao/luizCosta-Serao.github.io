50032a9f67cd28b9fe1ec8f4a8161cd98aa80677
Luiz Serão <luiz.serao@hotmail.com>
2024-01-17 08:10:14 -0300

    Mudança na seção Habilidades

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -16,24 +16,31 @@ const Skills = () => {
         <ul className={styles.skillsList}>
           <li>
             <img src={html} alt='HTML' />
+            <p>HTML</p>
           </li>
           <li>
             <img src={css} alt='CSS' />
+            <p>CSS</p>
           </li>
           <li>
             <img src={js} alt='JavaScript'/>
+            <p>JavaScript</p>
           </li>
           <li>
             <img src={react} alt='ReactJS' />
+            <p>ReactJS</p>
           </li>
           <li>
             <img src={figma} alt='Figma' />
+            <p>Figma</p>
           </li>
           <li>
             <img src={sass} alt='SASS' />
+            <p>Sass</p>
           </li>
           <li>
             <img src={github} alt='Github' />
+            <p>Github</p>
           </li>
         </ul>
       </div>
